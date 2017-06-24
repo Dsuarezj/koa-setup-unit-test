c7f04dd8fdb8f68b68be5a45db43e089bf903dc4
Denisse Suarez <dsuarez@thoughtworks.com>
2017-06-24 16:14:03 -0500

    Creates a endpoint that receive a post and use the name information to create a response.

---
+++ app.js
@@ -0,0 +1,25 @@
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const koaBody = require('koa-body');
+
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+app.use(logger());
+
+app.use(koaBody());
+
+router.post('/helloWorld', helloWorld)
+
+app.use(router.routes());
+
+async function helloWorld(ctx){
+  var recieveData = JSON.parse(ctx.request.body);
+  var name = recieveData["name"];
+  var message = "Hello " + name;
+  await (ctx.body = {"test":message})
+}
+
+app.listen(3000, function() {
+  console.log("El servidor Node.js se encuentra disponible en el puerto http://localhost:3000");
+});
